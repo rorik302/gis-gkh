30a379dfee59ae4ea5db2ef70b83a714f05602cb
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-06 13:27:18 +0300

    модель House

--- home_management/models.py
+++ home_management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    fias_id = models.CharField(max_length=50, default='')
+    guid = models.CharField(max_length=50, default='')
+    passport_info = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
