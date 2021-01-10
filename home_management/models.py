2512744145279703803c64976eb9325818b3f873
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-10 12:28:28 +0300

    поле passport_url

--- home_management/models.py
+++ home_management/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class House(models.Model):
     fias_id = models.CharField(max_length=50, default='')
     guid = models.CharField(max_length=50, default='')
+    passport_url = models.CharField(max_length=255, default='', null=True)
     passport_info = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
