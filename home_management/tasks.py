3df0e8561e481f846c869cc81c877ff32474d3d8
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-10 16:37:46 +0300

    задания на сохранения url и паспорта

--- home_management/tasks.py
+++ home_management/tasks.py
@@ -1,3 +1,6 @@
+import json
+
+from home_management.models import House
 from home_management.parsers import PassportParser, AddressParser
 
 
@@ -7,3 +10,18 @@ def get_passport_info(url):
 
 def get_passport_url(data):
     return AddressParser(data).get_passport_url()
+
+
+def get_and_save_passport_info(fias_id, url):
+    house = House.objects.get(fias_id=fias_id)
+    house.passport_info = json.dumps(get_passport_info(url), ensure_ascii=False)
+    house.save()
+
+
+def get_and_save_passport_url_and_info(fias_id, data):
+    url = get_passport_url(data)
+    passport_info = get_passport_info(url)
+    house = House.objects.get(fias_id=fias_id)[0]
+    house.passport_url = url
+    house.passport_info = json.dumps(passport_info, ensure_ascii=False)
+    house.save()
